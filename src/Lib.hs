2c3c9cead1360a89204aebe0815f33b51cc78d7c
santiagoparedes97 <santiagoparedes97@github.com>
2020-05-06 21:35:06 -0300

    Agrego la reputacion y la diferencia de estatura

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,22 @@
 module Lib where
 
-laVerdad = True
+type Estatura = Int
+type Hobbit = (String,Estatura)
+
+bilbo :: Hobbit
+bilbo = ("Bilbo Bolsonaro", 125)
+
+rosita :: Hobbit
+rosita = ("Rosita coto", 125)
+
+merryl :: Hobbit
+merryl = ("Meriadoc Dropdigamo",131)
+
+pippin :: Hobbit
+pippin = ("Peregrin Take",131)
+
+reputacion :: Hobbit -> Int
+reputacion hobbit = (length.fst) hobbit * (snd hobbit)
+
+diferenciaEnEstatura :: Hobbit -> Hobbit -> Int
+diferenciaEnEstatura (_,estatura) (_,otraEstatura) = estatura - otraEstatura
