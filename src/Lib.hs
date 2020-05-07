950e23c57bcff83207d30e3b11d854224cb0cea8
santiagoparedes97 <santiagoparedes97@github.com>
2020-05-06 21:51:01 -0300

    Cambiando tuplas por data :)

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,34 +3,34 @@ module Lib where
 type Estatura = Int
 type Fuerza = Int
 
-type Hobbit = (String,Estatura,Fuerza,Bool)
+data Hobbit = Hobbit String Estatura Fuerza Bool
 
 nombre :: Hobbit -> String
-nombre (nombre,_,_,_) = nombre
+nombre (Hobbit nombre _ _ _) = nombre
 
 estatura :: Hobbit -> Estatura
-estatura (_,estatura,_,_) = estatura
+estatura (Hobbit _ estatura _ _) = estatura
 
 bilbo :: Hobbit
-bilbo = ("Bilbo Bolsonaro", 125,30,True)
+bilbo = Hobbit "Bilbo Bolsonaro" 125 30 True
 
 rosita :: Hobbit
-rosita = ("Rosita coto", 125,40,True)
+rosita = Hobbit "Rosita coto" 125 40 True 
 
 merryl :: Hobbit
-merryl = ("Meriadoc Dropdigamo",131,50,True)
+merryl = Hobbit "Meriadoc Dropdigamo" 131 50 True
 
 pippin :: Hobbit
-pippin = ("Peregrin Take",131,50,True)
+pippin = Hobbit "Peregrin Take" 131 50 True
 
 reputacion :: Hobbit -> Int
 reputacion hobbit = (length.nombre) hobbit * (estatura hobbit)
 
 
 diferenciaEnEstatura :: Hobbit -> Hobbit -> Int
-diferenciaEnEstatura (_,estatura,_,_) (_,otraEstatura,_,_) = estatura - otraEstatura
+diferenciaEnEstatura (Hobbit _ estatura _ _) (Hobbit _ otraEstatura _ _) = estatura - otraEstatura
 
 
 capacidad :: Hobbit -> Int
-capacidad (nombre,_,fuerza,True) = (length nombre) * 3
-capacidad (nombre,estatura,_,False) = 10 + estatura
+capacidad (Hobbit nombre _ fuerza True) = (length nombre) * 3
+capacidad (Hobbit nombre estatura _ False) = 10 + estatura
